f698f06d1e51c25d2aa939d6cfb265f88ed60fbe
Sean Elliott <sqelliot@ucsc.edu>
2017-11-05 13:40:51 -0800

    compiles, board prints, test functioality

--- asg4/Hex.cpp
+++ asg4/Hex.cpp
@@ -1,26 +1,27 @@
 // Hex.cpp
 
 #include "Hex.h"
+#include <iostream>
 #include <stdexcept>
 #include <algorithm>
 using namespace std;
 
-Hex::Hex(int n = 11) : size(n){
+Hex::Hex(int n) : size(n){
   board.resize(get_size());
-  for(auto it=board.begin(); it!=board.end();++it){
-    *it.resize(get_size(),Player::none);
+  for(int i=0; i < get_size();++i){
+    board[i].resize(get_size(),Player::none);
   }
 }
 
 int Hex::make_move(int row, int col){
   int valid = 1;
-  if(game_over()){
+  if(is_game_over()){
     valid=-2;
   }
-  else if(!valid_move(row,col)){
+  else if(!valid_spot(row,col)){
     valid = -1;
   }
-  else if(!free_space(row,col)){
+  else if(!free_spot(row,col)){
     valid =  0;
   }
   else{
@@ -29,11 +30,12 @@ int Hex::make_move(int row, int col){
   return valid; 
 }
 
-bool Hex::winner(){
-  if(!game_over()){
-    did_player_won();
+bool Hex::check_for_winner(){
+  if(!is_game_over()){
+    did_player_win();
+    end_turn();
   }
-  return game_over();;
+  return is_game_over();;
 }
 
 int Hex::get_size() const{
@@ -44,24 +46,182 @@ Player Hex::curr_player() const{
   return this->player;
 }
 
+void Hex::display_board() const{
+  int i = 0;
+  for( int row = 0; row < get_size(); ++row, ++i){
+    for(int pad=0; pad < i; ++pad){cout << " ";}
+    for(int col = 0; col < get_size(); ++col){
+      Player p = player_on_spot(row,col);
+      switch(p){
+        case Player::red  : cout << "R";
+             break;
+        case Player::blue : cout << "B";
+             break;
+        default :
+                  cout << "U";
+                  break;
+      }
+      cout << " ";
+    }
+    cout << endl;
+  } 
+}
+
 /////////////////////
 // private methods //
 /////////////////////
 
-bool Hex::game_over() const{
-  return this->_game_over;
+// valid_spot()
+bool Hex::valid_spot(int r, int c) const{
+  if( r < 0 || r >= get_size() || c < 0 || c >= get_size()){
+    return false;
+  }
+  return true;
 }
+
+// free_spot()
+bool Hex::free_spot(int r,int c) const{
+  if( player_on_spot(r,c) != Player::none){
+    return false;
+  }
+  return true;
+}
+
+//player_on_spot()
+Player Hex::player_on_spot(int r, int c) const{
+  return this->board[r][c];
+}
+
+
+
+void Hex::add_move(int row, int col){
+   board[row][col] = curr_player();
+   if(Player::red == curr_player()){
+     if(row ==0){
+       this->red_walls.first.push_back(make_pair(row,col));
+     }
+     if(row == get_size()-1){
+       this->red_walls.second.push_back(make_pair(row,col));
+     } 
+   }
+   if(Player::blue == curr_player()){
+     if(col == 0){
+       this->blue_walls.first.push_back(make_pair(row,col));
+     }
+     if(col == get_size()-1){
+       this->blue_walls.second.push_back(make_pair(row,col));
+     }
+   }
+}
+
+void Hex::end_turn(){
+  if(Player::red == curr_player())
+    this->player = Player::blue;
+  else
+    this->player = Player::red;
+}
+
 
 bool Hex::did_player_win(){
   if(on_both_walls()){
-    auto spots = get_walls().first;;
-    Spots visited;
+    auto spots = get_wall();;
+    vector<Spot> visited;
+    vector<Spot> edge_queue;
+
+    // add all occupied spots by player 
     for(auto it = spots.cbegin(); it != spots.cend(); ++it){
-      if(visited.find(*it) != visited.end()){
-        visited.insert(*it);
+      edge_queue.push_back(*it);
+    }
+    while(!edge_queue.empty()){
+      Spot curr_spot = edge_queue.front();
+      auto it = find(visited.begin(), visited.end(), curr_spot);
+      if(it != visited.end()){
         
+        if(spot_connects_walls(curr_spot)){
+          game_is_over();
+          return is_game_over();
+        }
+
+        edge_queue.erase(edge_queue.begin());
+        visited.push_back(curr_spot);
+
+        vector<Spot> neigh = get_spots_neigh(curr_spot);
+        for(auto s : neigh){
+          auto v_it = find(visited.begin(), visited.end(), s);
+          auto e_it = find(edge_queue.begin(), edge_queue.end(),s);
+          if(v_it == visited.end() && e_it == edge_queue.end()){
+            edge_queue.push_back(s);
+          }
+        }
       }
     }
   }
-  return game_over();
+  return is_game_over();
 }
+
+void Hex::game_is_over(){
+  this->_game_over = true;
+  this->winner = curr_player();
+}
+
+bool Hex::is_game_over() const{
+  return this->_game_over;
+}
+
+vector<Spot> Hex::get_spots_neigh(Spot s){
+  int row = s.first;
+  int col = s.second;
+  int n_row, n_col;
+  vector<Spot> neigh;
+  for(int drow = -1; drow <2; ++drow){
+    for(int dcol = -1; dcol <2; ++dcol){
+      n_row = row+drow;
+      n_col = col+dcol;
+      if( valid_spot(n_row,n_col) && 
+          curr_player() == player_on_spot(n_row,n_col)){
+        neigh.push_back( make_pair(n_row,n_col));
+      }
+    }
+  }
+  return neigh;
+}
+
+
+bool Hex::on_both_walls(){
+  if(Player::red == curr_player()){
+    if(!(this->red_walls.first.empty() || 
+         this->red_walls.second.empty())){
+      return true;
+    }
+  }
+  else{
+    if(!(this->blue_walls.first.empty() ||
+         this->blue_walls.second.empty())){
+      return true;
+    }
+  }
+  return false;
+}
+
+bool Hex::spot_connects_walls(Spot s){
+  if(Player::red == curr_player()){
+    auto it = find(red_walls.second.begin(), red_walls.second.end(), s);
+    return (it != this->red_walls.second.end());
+  }
+  else{
+    auto it1 = find(blue_walls.second.begin(), blue_walls.second.end(), s);
+    return (it1 != this->blue_walls.second.end());
+  }
+}
+
+
+// get_walls()
+Spots Hex::get_wall(){
+  if(Player::red == curr_player()){
+    return this->red_walls.first;
+  }
+  else
+    return this->blue_walls.first;
+}
+
+
--- asg4/Hex.h
+++ asg4/Hex.h
@@ -2,10 +2,12 @@
 
 #include <vector>
 #include <unordered_set>
+#include "pair_hash.h"
 using namespace std;
 enum class Player{red,blue,none};
 typedef pair<int,int> Spot;
-typedef pair<unordered_set<Spot>,unordered_set<Spot>> Walls;
+typedef vector<Spot> Spots;
+typedef pair<Spots,Spots> Walls;
 
 class Hex{
 public:  
@@ -13,24 +15,43 @@ public:
   Hex(int n=11);
 
   // public methods
-  int make_move(Player,int,int);
-  bool winner() const;
+  int make_move(int,int);
+  bool check_for_winner();
 
   int get_size() const;
   Player curr_player() const;
+  bool is_game_over() const;
+
+  void display_board() const;
   
 private:
+  // state member variables
   vector<vector<Player>> board;
   Player player = Player::red;
+  Player winner = Player::none;
   bool _game_over = false;
   const int size;
+
+  // Container to maintain spots occupied by a player
+  // on their respective walls to win the game.
   Walls red_walls;
   Walls blue_walls;
   
+  // methods to check board 'Spots' logic
+  bool valid_spot(int,int) const;
+  bool free_spot(int,int) const;
+  Player player_on_spot(int,int) const;
 
-  void add_move();
-  bool game_over() const;
-  bool did_player_won();
+  void add_move(int,int );
+  void end_turn();
+  bool did_player_win();
+
+  void game_is_over();
+
+  vector<Spot> get_spots_neigh(Spot s);
   bool on_both_walls();
-  const Walls& get_walls();
+  bool spot_connects_walls(Spot);
+  Spots get_wall();
 };
+
+
